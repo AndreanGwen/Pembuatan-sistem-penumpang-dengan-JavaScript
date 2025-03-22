ed2ce0f84f0d40b4dea8e8384bf2ec080907009e
Andrean Gwen <rutherford@MacBook-Air-Andrean.local>
2025-03-22 18:27:55 +0700

    third commit

--- latihanjs.js
+++ latihanjs.js
@@ -1,7 +1,40 @@
-var buahBuahan = ["Mangga", "Apel", "Pisang"];
-var apakahAda = function (namaBuah, buahBuahan) {
-  if (buahBuahan.includes(namaBuah)) {
-    console.log("Buah dengan nama " + namaBuah + " tersedia");
-    return;
+var penumpang = [];
+
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    penumpang.push(namaPenumpang);
+    return penumpang;
+  } else {
+    for (i = 0; i < 15; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log(
+          "Penumpang dengan nama " + namaPenumpang + " sudah berada dikursi"
+        );
+        return penumpang;
+      } else if (penumpang.length >= 15) {
+        console.log("Penumpang sudah penuh!");
+        return penumpang;
+      }
+    }
+  }
+};
+
+var hapusPenumpang = function (namaPenumpang, penumpang) {
+  if (penumpang.length == 0) {
+    console.log("Angkot masih kosong!");
+    return penumpang;
+  } else {
+    for (i = 0; i < 15; i++) {
+      if (penumpang[i] == namaPenumpang) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (!penumpang.includes(namaPenumpang)) {
+        console.log("Tidak ada penumpang dengan nama " + namaPenumpang);
+        return penumpang;
+      }
+    }
   }
 };
