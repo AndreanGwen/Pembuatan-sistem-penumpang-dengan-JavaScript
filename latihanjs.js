2c08587a8c70078aa6323e094f49bd85a347d9bd
Andrean Gwen <rutherford@MacBook-Air-Andrean.local>
2025-03-22 18:25:22 +0700

    first commit

---
+++ latihanjs.js
@@ -0,0 +1,7 @@
+var buahBuahan = ["Mangga", "Apel", "Pisang"];
+var apakahAda = function (namaBuah, buahBuahan) {
+  if (buahBuahan.includes(namaBuah)) {
+    console.log("Buah dengan nama " + namaBuah + " tersedia");
+    return;
+  }
+};
